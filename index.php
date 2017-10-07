287788bad7f602f000237ed8b9f8915a2cc4f80a
Egor <evn88fx64@gmail.com>
2017-10-07 13:33:44 +0300

    add style index

--- index.php
+++ index.php
@@ -454,7 +454,7 @@ $APPLICATION->SetTitle("Калькулятор технологического 
             <p class="in_2 check">
                 <label>
                     <input type="checkbox" value="check" v-model="BuildTP">
-                    <span class="jq-checkbox" :class="{ checked: BuildTP }"></span> Строительство ТП <span class="">{{(j)?j.Z.TP : ""}}</span>
+                    <span class="jq-checkbox" :class="{ checked: BuildTP }"></span> Строительство ТП <span class="sqrt">{{(j)?j.Z.TP : ""}}</span>
                 </label>
             </p>
             <div class="inner_wrap" v-show="BuildTP">
